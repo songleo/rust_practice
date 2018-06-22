c0d75d2edfb658ae43f25d6264f519d08674485a
LEo <lisong1205@126.com>
2018-06-22 18:14:57 +0800

    auto commit

--- hello_word/src/main.rs
+++ hello_word/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let x = vec![1, 2, 3];
+
+    let x = print_vector(x);
+
+    println!("{:?}", x);
+}
+
+fn print_vector(v: Vec<i32>) -> Vec<i32> {
+   println!("I took this data: {:?}, and returned it", v);
+   v
 }
