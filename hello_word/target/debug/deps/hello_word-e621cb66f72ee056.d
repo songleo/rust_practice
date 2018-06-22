775e83b21f300335cff7a0373506db8bca1c76d1
LEo <lisong1205@126.com>
2018-06-22 18:01:02 +0800

    auto commit

--- hello_word/target/debug/deps/hello_word-e621cb66f72ee056.d
+++
@@ -1,5 +0,0 @@
-/media/sf_share/git/rust_practice/hello_word/target/debug/deps/hello_word-e621cb66f72ee056: src/main.rs
-
-/media/sf_share/git/rust_practice/hello_word/target/debug/deps/hello_word-e621cb66f72ee056.d: src/main.rs
-
-src/main.rs:
--- hello_word/target/debug/hello_word.d
+++
@@ -1,1 +0,0 @@
-/media/sf_share/git/rust_practice/hello_word/target/debug/hello_word: /media/sf_share/git/rust_practice/hello_word/src/main.rs
